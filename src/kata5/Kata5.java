6393070346d6b03c955db0a6f46835ffa499ca96
usuario <usuario@10.22.146.203>
2013-11-15 11:55:55 +0000

    Segunda Implementación, ya tiene AttributeExtractor

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,17 +1,17 @@
 package kata5;
 
 public class Kata5 {
+
     public static void main(String[] args) {
         MockMailLoader loader = new MockMailLoader();
         HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
-        builder.build(new AttributeExtractor<Mail, String>() {
-
+        Histogram<String> histogram = builder.build(new AttributeExtractor<Mail, String>() {
             @Override
             public String extract(Mail entity) {
                 return entity.getDomain();
             }
         });
-        HistogramViewer<Mail> viewer = new HistogramViewer<>();
-        viewer.show(builder.getHistogram());
+        HistogramViewer<String> viewer = new HistogramViewer<>();
+        viewer.show(histogram);
     }
 }
